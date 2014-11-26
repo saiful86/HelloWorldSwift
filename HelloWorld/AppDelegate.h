5a3764e7e07615c92c541101830e4acd380b6faa
sr0811 <saiful.ariful@yahoo.com>
2014-11-27 01:14:12 +0600

    first commit

---
+++ HelloWorld/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HelloWorld
+//
+//  Created by SAIFUL ISLAM on 10/1/14.
+//  Copyright (c) 2014 SAIFUL ISLAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by SAIFUL ISLAM on 10/1/14.
+//  Copyright (c) 2014 SAIFUL ISLAM. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
